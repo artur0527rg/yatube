3e61e210b42441c42ef833f695ed49f6b774863f
ARTUR <rogozinskiyartur@gmail,com>
2022-08-13 10:25:44 +0300

    First commit(Перед созданием приложения User)

---
+++ yatube/post/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post, Group
+
+# Create your views here.
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+# view-функция для страницы сообщества
+def group_posts(request, slug):
+    # функция get_object_or_404 получает по заданным критериям объект из базы данных 
+    def get_object_or_404(group, slug):
+        try:
+            object = Group.objects.get(slug = slug)
+            return object
+        except :
+            pass
+    
+    # или возвращает сообщение об ошибке, если объект не найден
+    group = get_object_or_404(Group, slug=slug)
+    
+    # # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
+    # # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
