0fdb710b92a6862e1954a29e11f80686934ff74d
ARTUR <rogozinskiyartur@gmail,com>
2022-09-18 00:31:00 +0300

    add user_filter

--- yatube/Users/templatetags/user_filters.py
+++ yatube/Users/templatetags/user_filters.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from django import template
 # В template.Library зарегистрированы все теги и фильтры шаблонов
 # добавляем к ним и наш фильтр
@@ -11,3 +12,17 @@ def addclass(field, css):
 # синтаксис @register... , под которой описан класс addclass() - 
 # это применение "декораторов", функций, обрабатывающих функции
 # мы скоро про них расскажем. Не бойтесь соб@к
+
+@register.filter
+def uglify(field):
+    dlin = len(field)
+    new_str = ''
+    for i in range(0, dlin):
+        if i%2!=0:
+            new_str += field[i].upper()
+        if i%2==0:
+            new_str += field[i].lower()
+        
+            
+    return new_str
+        
