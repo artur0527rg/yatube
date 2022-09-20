edb8651a6c4e8b4bd36100d8c6eb15cee5c2311d
ARTUR <rogozinskiyartur@gmail,com>
2022-09-20 15:54:30 +0300

    Имитация почтового ящика в  sent_emails

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -135,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index" 
 # LOGOUT_REDIRECT_URL = "index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
