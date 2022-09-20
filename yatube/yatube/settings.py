e7df4158af663b299d17bcc65fb8d208db38bbea
ARTUR <rogozinskiyartur@gmail,com>
2022-09-20 15:12:44 +0300

    Добавил шаблоны для станадртных путей django(login, logout)

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -129,3 +129,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index" 
+# LOGOUT_REDIRECT_URL = "index"
