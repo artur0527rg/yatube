e94ed1ffcc7a017633fb6dbcd59d9ef6f8d119db
ARTUR <rogozinskiyartur@gmail,com>
2022-10-02 20:26:10 +0300

    Пагинатор на стриницах групп

--- yatube/post/views.py
+++ yatube/post/views.py
@@ -11,7 +11,6 @@ from .forms import PostForm
 def index(request):
         post_list = Post.objects.order_by('-pub_date').all()
         paginator = Paginator(post_list, 10)  # показывать по 10 записей на странице.
-
         page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
         page = paginator.get_page(page_number)  # получить записи с нужным смещением
         return render(
@@ -23,13 +22,19 @@ def index(request):
 # view-функция для страницы сообщества
 def group_posts(request, slug):
     # функция get_object_or_404 получает по заданным критериям объект из базы данных
-    # # или возвращает сообщение об ошибке, если объект не найден
+    # или возвращает сообщение об ошибке, если объект не найден
     group = get_object_or_404(Group, slug=slug)
     
-    # # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
-    # # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, "group.html", {"group": group, "posts": posts})
+    # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
+    post_list = Post.objects.filter(group=group).order_by("-pub_date")
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')  # переменная в URL с номером запрошенной страницы
+    page = paginator.get_page(page_number)  
+    return render(
+        request,
+        "group.html",
+        {"group": group, 'page': page, 'paginator': paginator}
+    )
 
 def new_post(request):
     if request.method == 'POST':
